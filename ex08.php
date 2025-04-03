fd8ff354cd8578d0257a707b278d7bf16025bee6
Hugo <hugo.thivel@gmail.com>
2025-04-03 10:14:10 +0200

    done

--- ex08.php
+++ ex08.php
@@ -6,3 +6,11 @@
 //   concaténer caractère à une nouvelle chaîne
 // afficher la nouvelle chaîne
 
+$name = 'Hugo';
+$invertedName = '';
+
+for ($i = strlen($name) - 1; $i >= 0; $i--) {
+  $invertedName .= $name[$i];
+}
+
+echo $invertedName;
