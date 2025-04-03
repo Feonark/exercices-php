5ad5a7862e3f657ccdcb6fa4d2cf3111720b7b9c
Hugo <hugo.thivel@gmail.com>
2025-04-03 10:41:37 +0200

    done

--- ex10.php
+++ ex10.php
@@ -8,3 +8,14 @@
 //   n = n // 10
 // afficher somme
 
+$sum = 0;
+$n = 442;
+
+echo 'La somme des chiffres de ' . $n;
+
+while ($n > 0) {
+  $sum += $n % 10;
+  $n = intdiv($n, 10);
+}
+
+echo ' est : ' . $sum;
